ead98ff92625d4109c0f54b220a3da130b2adf0b
Christian <ctrouse@ucsc.edu>
2021-04-21 17:31:20 -0700

    Reupload Patch

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// Christian Rouse
+// Rocket Patrol
+// 16 April 2021
+// Time: Approximately 7 hours
+let config = {
+  type: Phaser.AUTO,
+  width: 640,
+  height: 480,
+  scene: [Menu, Play]
+}
+let game = new Phaser.Game(config);
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
