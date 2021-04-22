3f8f6d2d59e9c5bacc331e72d472c538fcb61c20
Christian <ctrouse@ucsc.edu>
2021-04-21 19:16:56 -0700

    Update Commit

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@
 // Time: Approximately 7 hours
 let config = {
   type: Phaser.AUTO,
-  width: 1280,
-  height: 720,
+  width: 720,
+  height: 480,
   scene: [Menu, Play]
 }
 let game = new Phaser.Game(config);
