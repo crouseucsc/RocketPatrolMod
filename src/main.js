b4ad8ddc9cbd7cbd4180d059e429231188a6c6ea
Christian <ctrouse@ucsc.edu>
2021-04-21 19:54:25 -0700

    Mod Main.js update

--- src/main.js
+++ src/main.js
@@ -11,19 +11,3 @@ let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
 let keyLEFT, keyRIGHT, keyUP, keyW, keyR, keyA, keyD;
-
-/*
-Rutvik Katkoriya, Rocket Patrol: 2-Player Edition, ~15 hours
-
-Points Breakdwon
-  30 Points: Simultaneous 2-player mode
-  20 Points: New spaceship asset (faster, smaller, more points)
-  10 Points: New title screen
-  10 Points: Parallax scrolling
-  5 Points: FIRE UI text in Play scene
-  5 Points: Wide screen
-  5 Points: Novice/Expert voice SFX
-  5 Points: Winner display at end of Play scene
-  5 Points: Original music in Play scene
-  5 Points: New background art
-*/
