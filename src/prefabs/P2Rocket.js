e9d464a9e78b190c8808023f0841829da75ab6fc
Christian <ctrouse@ucsc.edu>
2021-05-14 21:30:30 -0700

    Test6.2

--- src/prefabs/P2Rocket.js
+++ src/prefabs/P2Rocket.js
@@ -2,7 +2,7 @@ class P2Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
-        this.movementSpeed = 3;
+        this.movementSpeed = 6;
         this.isFiring = false;
         this.sfxRocket = scene.sound.add('sfx_rocket');
     }
