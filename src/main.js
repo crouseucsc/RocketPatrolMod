ab7a80d300e765426c65826994d91a9b4bce2ac6
Christian <ctrouse@ucsc.edu>
2021-05-14 21:01:56 -0700

    Test2

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@
 //Time: 20hrs+
 //19 April 2021 17:00
 let config = {
-  type: Phaser.AUTO,
-  width: 1920,
-  height: 1080,
+  type: Phaser.CANVAS,
+  width: 1280,
+  height: 720,
   scene: [Menu, Play],
 }
 
 let game = new Phaser.Game(config);
 
-let borderUISize = game.config.height / 15;
+let borderUISize = game.config.height / 30;
 let borderPadding = borderUISize / 3;
 
 let keyLEFT, keyRIGHT, keyUP, keyW, keyR, keyA, keyD;
