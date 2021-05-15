a2cf1364e4aa11bacbe0bdefc60bbc1dbf5f55e4
Christian <ctrouse@ucsc.edu>
2021-05-14 21:29:27 -0700

    Test6.1

--- src/main.js
+++ src/main.js
@@ -13,6 +13,6 @@ let config = {
 let game = new Phaser.Game(config);
 
 let borderUISize = game.config.height / 75;
-let borderPadding = borderUISize / 2;
+let borderPadding = borderUISize / 3;
 
 let keyLEFT, keyRIGHT, keyUP, keyW, keyR, keyA, keyD;
