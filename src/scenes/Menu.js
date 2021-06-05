7c53a3c4c2cace17d1c06d8f85391c0d9d18ef9a
Christian <ctrouse@ucsc.edu>
2021-06-05 00:31:32 -0700

    Customize Menu

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -23,27 +23,27 @@ class Menu extends Phaser.Scene {
         }
         this.add.text(game.config.width / 2, game.config.height / 2 - borderUISize - borderPadding, 'ROCKET PATROL MOD REDUX (WIP)', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width / 2, game.config.height / 2, 'Use ←→ to move & (F) to fire.', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = '#00FF00';
-        menuConfig.color = '#000';
-        this.add.text(game.config.width / 2, game.config.height / 2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
+        menuConfig.backgroundColor = '#0000FF';
+        menuConfig.color = '#FFFF00';
+        this.add.text(game.config.width / 2, game.config.height / 2 + borderUISize + borderPadding, 'Press ← for Easy or → for Hard', menuConfig).setOrigin(0.5);
         // define keys
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
     update() {
         if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
-            // easy mode
+            // easy/beginner mode
             game.settings = {
-                spaceshipSpeed: 3,
+                spaceshipSpeed: 3, //independent ship speed. 
                 gameTimer: 60000
             }
             this.sound.play('sfx_select');
             this.scene.start('playScene');
         }
         if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
-            // hard mode
+            // hard/expert mode
             game.settings = {
-                spaceshipSpeed: 5,
+                spaceshipSpeed: 5,//independent ship speed. 
                 gameTimer: 45000
             }
             this.sound.play('sfx_select');
