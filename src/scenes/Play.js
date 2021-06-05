c9a4c03dce29eac8f0f646fc872fef9096b42d1f
Christian <ctrouse@ucsc.edu>
2021-06-05 02:54:44 -0700

    Coordinate Change

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -26,7 +26,7 @@ class Play extends Phaser.Scene {
         this.ship01 = new Spaceship(this, game.config.width + borderUISize * 8, borderUISize * 4, 'spaceship', 0, 30).setOrigin(0, 0);
         this.ship02 = new Spaceship(this, game.config.width + borderUISize * 3, borderUISize * 5 + borderPadding * 2, 'spaceship', 0, 20).setOrigin(0, 0);
         this.ship03 = new Spaceship(this, game.config.width, borderUISize * 8 + borderPadding * 4, 'spaceship', 0, 10).setOrigin(0, 0);
-        this.ship04 = new Bomber (this, game.config.width + borderUISize * 10, borderUISize * 6, 'bomber', 0, 70).setOrigin(0, 0); 
+        this.ship04 = new Bomber (this, game.config.width + borderUISize * 10, borderUISize * 6, 'bomber', 0, 0).setOrigin(0, 0); 
         // define keys (p1)
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
