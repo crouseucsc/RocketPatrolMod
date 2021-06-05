e6534317498b2926eadee8a279d395ca00113eff
Christian <ctrouse@ucsc.edu>
2021-06-05 00:17:09 -0700

    Basic Text Update

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -21,7 +21,7 @@ class Menu extends Phaser.Scene {
             },
             fixedWidth: 0
         }
-        this.add.text(game.config.width / 2, game.config.height / 2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 2 - borderUISize - borderPadding, 'ROCKET PATROL MOD REDUX (WIP)', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width / 2, game.config.height / 2, 'Use ←→ to move & (F) to fire.', menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#00FF00';
         menuConfig.color = '#000';
