62f0b0969a1d03287ad594aebbedc9ddbeac8d59
Christian <ctrouse@ucsc.edu>
2021-06-04 23:31:26 -0700

    Experimentation

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,7 +11,7 @@ class Play extends Phaser.Scene {
     }
     create() {
         this.starfield = this.add.tileSprite(0, 0, 720, 480, 'starfield').setOrigin(0, 0);
-        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0xFF0000).setOrigin(0, 0);
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 4, 0xFF0000).setOrigin(0, 0);
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFF0000).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFF0000).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFF0000).setOrigin(0, 0);
